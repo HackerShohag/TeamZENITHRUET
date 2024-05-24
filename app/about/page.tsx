eca0bdc69fc897945e4dec92de078abe79dc4a15
HackerShohag <HackerShohag@outlook.com>
2024-05-25 03:00:18 +0600

    some fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,17 +8,17 @@ import annexeWhiteLogo from '@/public/annexe-white.png';
 export default function AboutPage() {
 	return (
 		<div>
-			<h1 className={title()}>Team Annexe RUET</h1>
+			<h1 className={title({ className: "font-black" })}>
+				Team ANNEXE RUET
+			</h1>
 
-			{/* Team Annexe RUET */}
-			{/* <h2 className="text-2xl py-5"></h2> */}
 			<div className="grid grid-cols-1 sm:grid-cols-2 max-w-6xl my-10">
 				<div className="flex w-full border rounded-md items-center justify-center">
 					<Image classNames={{ wrapper: "hidden dark:sm:flex" }} className="hidden dark:flex w-auto" width={700} src={annexeWhiteLogo.src} alt="Annexe Logo" />
 					<Image classNames={{ wrapper: "dark:hidden" }} className="dark:hidden w-auto" width={700} src={annexeDarkLogo.src} alt="Annexe Logo" />
 				</div>
 				<div>
-					<h2 className="text-lg py-5 text-left mx-5">
+					<h2 className="text-lg py-5 text-justify mx-5">
 						We are a passionate and ambitious student team at RUET (Rajshahi University of Engineering & Technology), specializing in automotive projects including Formula Student Go Kart and NASA Human Rover challenges. With dedication and creativity at their core, they tirelessly pursue their dreams, pushing the boundaries of innovation in engineering. Through teamwork and perseverance, Team Annexe strives to make a mark in the world of automotive design and competition, embodying the spirit of determination and ingenuity.
 					</h2>
 				</div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,14 @@
+import { title } from "@/components/primitives";
 import Project from "@/components/project";
 import { projects } from "@/lib/data/projects";
 
 export default function ProjectsPage() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold text-center my-4">Projects</h1>
+			<h1 className={title({ className: "font-black" })}>
+				Projects
+			</h1>
+			<h1 className="text-3xl font-bold text-center my-4"></h1>
 			<div className="flex flex-col gap-5">
 				{
 					projects.map((project, index) => (
