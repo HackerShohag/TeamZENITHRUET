3d6b7d78a44daaac5d918674ac5e77c5506971fd
HackerShohag <HackerShohag@outlook.com>
2024-05-25 03:04:23 +0600

    some fix 2

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,11 +4,10 @@ import { projects } from "@/lib/data/projects";
 
 export default function ProjectsPage() {
 	return (
-		<>
-			<h1 className={title({ className: "font-black" })}>
+		<div className="flex flex-col">
+			<h1 className={title({ className: "font-black mb-10" })}>
 				Projects
 			</h1>
-			<h1 className="text-3xl font-bold text-center my-4"></h1>
 			<div className="flex flex-col gap-5">
 				{
 					projects.map((project, index) => (
@@ -16,6 +15,6 @@ export default function ProjectsPage() {
 					))
 				}
 			</div>
-		</>
+		</div>
 	);
 }
